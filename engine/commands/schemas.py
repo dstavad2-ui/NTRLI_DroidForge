16420c7a5c290901fb8c12cf55bb3fb0efa35ab7
dstavad2-ui <dstavad2@gmail.com>
2025-12-23 21:35:16 +0100

    Create schemas.py

---
+++ engine/commands/schemas.py
@@ -0,0 +1,27 @@
+COMMAND_SCHEMAS = {
+    "MODE": [
+        "STATELESS",
+        "STATEFUL",
+        "ANALYSIS",
+        "EXECUTION",
+        "DRY_RUN",
+        "VERBOSE"
+    ],
+    "INTENT": [
+        "ANALYZE",
+        "BUILD_APK",
+        "VALIDATE",
+        "SYNTHESIZE",
+        "ORCHESTRATE",
+        "SELF_ANALYZE",
+        "SELF_IMPROVEMENT_PLAN",
+        "APPLY_IMPROVEMENT",
+        "LIST_COMMANDS",
+        "LIST_AI_PROVIDERS"
+    ],
+    "AUTHORIZE": [
+        "EXECUTION",
+        "MUTATION",
+        "STATE_PERSISTENCE"
+    ]
+}
